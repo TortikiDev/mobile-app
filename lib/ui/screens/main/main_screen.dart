2c8aa52433153bd47a315494583b03de1932c1a9
Никита Красавин <6nike6@bk.ru>
2020-12-02 23:29:31 +0300

    handle create entity button tap

--- lib/ui/screens/main/main_screen.dart
+++ lib/ui/screens/main/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../app_localizations.dart';
 import '../../../bloc/main/index.dart';
+import '../../reusable/in_develop_screen_factory.dart';
 import '../../reusable/widget_factory.dart';
 
 class MainScreen extends StatelessWidget {
@@ -36,8 +37,30 @@ class MainScreen extends StatelessWidget {
               ]),
               floatingActionButton: state.showCreatePostButton
                   ? FloatingActionButton(
-                      child: Icon(Icons.edit), onPressed: () {})
+                      child: Icon(Icons.edit),
+                      onPressed: () => _onCreateEntityTap(context))
                   : null)),
     );
+  }
+
+  void _onCreateEntityTap(BuildContext context) {
+    final tabController = DefaultTabController.of(context);
+    final tabIndex = tabController.index;
+    WidgetFactory createEntityScreenFactory;
+    switch (tabIndex) {
+      case 0:
+        createEntityScreenFactory = InDevelopWidgetFactory();
+        break;
+      case 1:
+        createEntityScreenFactory = InDevelopWidgetFactory();
+        break;
+      default:
+        throw IndexError(tabIndex, tabController);
+    }
+
+    final pageRoute = MaterialPageRoute(
+        builder: (context) => createEntityScreenFactory.createWidget(),
+        fullscreenDialog: true);
+    Navigator.of(context).push(pageRoute);
   }
 }
