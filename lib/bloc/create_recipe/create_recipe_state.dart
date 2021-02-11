3357d04f3a9a6c27e35426412418e5a23473e6ea
Никита Красавин <6nike6@bk.ru>
2021-02-11 23:39:24 +0300

    add create recipe state fields

--- lib/bloc/create_recipe/create_recipe_state.dart
+++ lib/bloc/create_recipe/create_recipe_state.dart
@@ -1,28 +1,50 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 
 class CreateRecipeState extends Equatable {
   final String title;
+  final double complexity;
+  final List<String> _ingredients;
+  final String cookingSteps;
+  final List<File> _photos;
   final bool canCreateRecipe;
   final bool creatingRecipe;
   final bool recipeSuccessfulyCreated;
 
+  List<String> get ingredients => List.of(_ingredients);
+  List<File> get photos => List.of(_photos);
+
   const CreateRecipeState({
     this.title = '',
+    this.complexity = 3.0,
+    List<String> ingredients = const [],
+    this.cookingSteps = '',
+    List<File> photos = const [],
     this.canCreateRecipe = false,
     this.creatingRecipe = false,
     this.recipeSuccessfulyCreated = false,
-  });
+  })  : _ingredients = ingredients,
+        _photos = photos;
 
   factory CreateRecipeState.initial() => CreateRecipeState();
 
   CreateRecipeState copy({
     String title,
+    double complexity,
+    List<String> ingredients,
+    String cookingSteps,
+    List<File> photos,
     bool canCreateRecipe,
     bool creatingRecipe,
     bool recipeSuccessfulyCreated,
   }) =>
       CreateRecipeState(
         title: title ?? this.title,
+        complexity: complexity ?? this.complexity,
+        ingredients: ingredients ?? _ingredients,
+        cookingSteps: cookingSteps ?? this.cookingSteps,
+        photos: photos ?? _photos,
         canCreateRecipe: canCreateRecipe ?? this.canCreateRecipe,
         creatingRecipe: creatingRecipe ?? this.creatingRecipe,
         recipeSuccessfulyCreated:
@@ -32,6 +54,10 @@ class CreateRecipeState extends Equatable {
   @override
   List<Object> get props => [
         title,
+        complexity,
+        ingredients,
+        cookingSteps,
+        photos,
         canCreateRecipe,
         creatingRecipe,
         recipeSuccessfulyCreated,
