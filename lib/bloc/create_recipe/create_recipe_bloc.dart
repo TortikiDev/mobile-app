e4a6e3c6317586e1a41cd4d1685ddc1bf2892def
Никита Красавин <6nike6@bk.ru>
2021-02-25 23:03:39 +0300

    add ccan create recipe validation logic

--- lib/bloc/create_recipe/create_recipe_bloc.dart
+++ lib/bloc/create_recipe/create_recipe_bloc.dart
@@ -24,6 +24,7 @@ class CreateRecipeBloc extends BaseBloc<CreateRecipeEvent, CreateRecipeState> {
     if (event is BlocInit) {
     } else if (event is TitleChanged) {
       yield state.copy(title: event.text);
+      yield state.copy(canCreateRecipe: _canCreateRecipe());
     } else if (event is PlusComplexity) {
       final complexity = min(state.complexity + 0.5, 5.0);
       yield state.copy(complexity: complexity);
@@ -32,19 +33,35 @@ class CreateRecipeBloc extends BaseBloc<CreateRecipeEvent, CreateRecipeState> {
       yield state.copy(complexity: complexity);
     } else if (event is DescriptionChanged) {
       yield state.copy(description: event.text);
+      yield state.copy(canCreateRecipe: _canCreateRecipe());
     } else if (event is IngredientsChanged) {
       yield state.copy(ingredients: event.ingredients);
+      yield state.copy(canCreateRecipe: _canCreateRecipe());
     } else if (event is CookingStepsChanged) {
       yield state.copy(cookingSteps: event.text);
+      yield state.copy(canCreateRecipe: _canCreateRecipe());
     } else if (event is PhotoPicked) {
       final updatedPhotos = state.photos + [event.photo];
       yield state.copy(photos: updatedPhotos);
+      yield state.copy(canCreateRecipe: _canCreateRecipe());
     } else if (event is PhotoDeleted) {
       final updatedPhotos = state.photos;
       updatedPhotos.removeWhere((e) => e == event.photo);
       yield state.copy(photos: updatedPhotos);
+      yield state.copy(canCreateRecipe: _canCreateRecipe());
     } else if (event is CreateRecipe) {}
   }
+
+  // endregion
+
+  // region Private methods
+
+  bool _canCreateRecipe() =>
+      state.title.length > 3 &&
+      state.description.isNotEmpty &&
+      state.ingredients.isNotEmpty &&
+      state.cookingSteps.length > 3 &&
+      state.photos.isNotEmpty;
 
   // endregion
 }
