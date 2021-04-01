998cf8fe9d87548b38b2075798e73830636b7b7a
Никита Красавин <6nike6@bk.ru>
2021-04-01 23:31:06 +0300

    add confectioner bottom panel animation and swipe to dismiss behavior

--- lib/ui/screens/map/map_screen.dart
+++ lib/ui/screens/map/map_screen.dart
@@ -18,9 +18,15 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen>
-    with AutomaticKeepAliveClientMixin<MapScreen> {
+    with
+        AutomaticKeepAliveClientMixin<MapScreen>,
+        SingleTickerProviderStateMixin {
   final _mapController = MapController();
   StreamSubscription _mapChangeSubscription;
+  ConfectionerShortResponse _selectedConfectioner;
+
+  AnimationController _confectionerPanelAnimationController;
+  Animation<Offset> _confectionerPanelOffsetAnimation;
 
   @override
   bool get wantKeepAlive => true;
@@ -29,9 +35,32 @@ class _MapScreenState extends State<MapScreen>
   void initState() {
     super.initState();
     _mapChangeSubscription = _mapController.mapEventStream.listen((event) {
+      if (_confectionerPanelAnimationController.status ==
+          AnimationStatus.completed) {
+        _confectionerPanelAnimationController.reverse();
+      }
       final mapCenter = event.center;
       BlocProvider.of<MapBloc>(context).add(UpdateMapCenter(mapCenter));
     });
+
+    _confectionerPanelAnimationController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _confectionerPanelAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          _selectedConfectioner = null;
+        });
+      }
+    });
+    _confectionerPanelOffsetAnimation = Tween<Offset>(
+      begin: Offset(0.0, 0.0),
+      end: Offset(0.0, -1.2),
+    ).animate(CurvedAnimation(
+      parent: _confectionerPanelAnimationController,
+      curve: Curves.easeOutSine,
+    ));
   }
 
   @override
@@ -51,21 +80,47 @@ class _MapScreenState extends State<MapScreen>
       final confectionerMarkers =
           _getConfectionerMarkers(state.confectioners, theme);
 
-      return FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(
-          // TODO: use actual user's coordinates
-          center: LatLng(54.602, 39.862),
-          zoom: 16.5,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c'],
+      return Stack(
+        children: [
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              // TODO: use actual user's coordinates
+              center: LatLng(54.602, 39.862),
+              zoom: 16.5,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+              ),
+              MarkerLayerOptions(
+                markers: confectionerMarkers,
+              ),
+            ],
           ),
-          MarkerLayerOptions(
-            markers: confectionerMarkers,
-          ),
+          if (_selectedConfectioner != null)
+            Positioned(
+              bottom: -64,
+              left: 0,
+              right: 0,
+              child: SlideTransition(
+                position: _confectionerPanelOffsetAnimation,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Dismissible(
+                    key: Key('Confectioner panel dismisible'),
+                    direction: DismissDirection.down,
+                    onDismissed: (direction) =>
+                        _confectionerPanelAnimationController.reverse(),
+                    child: _ConfectionerPanel(
+                      confectioner: _selectedConfectioner,
+                    ),
+                  ),
+                ),
+              ),
+            ),
         ],
       );
     });
@@ -75,28 +130,60 @@ class _MapScreenState extends State<MapScreen>
           List<ConfectionerShortResponse> confectioners, ThemeData theme) =>
       confectioners
           .map(
-            (e) => Marker(
+            (conf) => Marker(
               width: 48,
               height: 48,
-              point: LatLng(e.coordinate.lat, e.coordinate.long),
-              builder: (context) => CircleAvatar(
-                radius: 48,
-                backgroundColor: theme.colorScheme.primaryVariant,
-                child: e.avatarUrl?.isValidUrl() ?? false
-                    ? CachedNetworkImage(
-                        key: Key(e.avatarUrl),
-                        imageUrl: e.avatarUrl,
-                        imageBuilder: (context, imageProvider) {
-                          return CircleAvatar(
-                              radius: 21,
-                              backgroundColor: Colors.transparent,
-                              backgroundImage: imageProvider);
-                        },
-                        fit: BoxFit.cover,
-                      )
-                    : Container(color: Colors.grey[300]),
+              point: LatLng(conf.coordinate.lat, conf.coordinate.long),
+              builder: (context) => GestureDetector(
+                onTap: () => _tapOnConfectionerMarker(conf),
+                child: CircleAvatar(
+                  radius: 48,
+                  backgroundColor: theme.colorScheme.primaryVariant,
+                  child: conf.avatarUrl?.isValidUrl() ?? false
+                      ? CachedNetworkImage(
+                          key: Key(conf.avatarUrl),
+                          imageUrl: conf.avatarUrl,
+                          imageBuilder: (context, imageProvider) {
+                            return CircleAvatar(
+                                radius: 21,
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: imageProvider);
+                          },
+                          fit: BoxFit.cover,
+                        )
+                      : Container(color: Colors.grey[300]),
+                ),
               ),
             ),
           )
           .toList();
+
+  void _tapOnConfectionerMarker(ConfectionerShortResponse confectioner) {
+    setState(() {
+      _selectedConfectioner = confectioner;
+    });
+    _confectionerPanelAnimationController.forward();
+  }
+}
+
+class _ConfectionerPanel extends StatelessWidget {
+  final ConfectionerShortResponse confectioner;
+  const _ConfectionerPanel({
+    Key key,
+    @required this.confectioner,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 64,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+        color: Colors.white,
+      ),
+      child: Center(
+        child: Text(confectioner.name),
+      ),
+    );
+  }
 }
